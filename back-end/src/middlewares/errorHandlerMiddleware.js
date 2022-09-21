c96e0cc79f98922ff46c2b76304e2aff2cb9f90c
evandro663 <evaandro@msn.com>
2022-09-21 15:22:45 -0300

    add middleware auth e errors

---
+++ back-end/src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,21 @@
+const errors = {
+  ValidationError: 400,
+  UnauthorizedError: 401,
+  NotFoundError: 404,
+  SequelizeUniqueConstraintError: 409,
+};
+
+/**
+ * 
+ * @param {Error} err 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @param {import('express').NextFunction} next 
+ */
+const errorHandlerMiddleware = ({ name, message }, _req, res, _next) => { // {name, message} desestruturação de "ERR"
+  const status = errors[name];
+  if (!status) return res.sendStatus(500);
+  res.status(status).json({ message });
+};
+
+module.exports = errorHandlerMiddleware;
