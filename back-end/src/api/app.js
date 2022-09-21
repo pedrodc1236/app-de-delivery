8d3dc4e592d379e64471724eb8c483e11dcdc680
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-09-21 18:04:15 -0300

    feat:  crud de produtos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+require('express-async-errors');
+const productRoute = require('../routes/productRoute');
+const errorHandlerMiddleware = require('../middleware/errorHandler');
 
 const app = express();
+
+app.use(express.json());
+app.use('/products', productRoute);
+app.use(errorHandlerMiddleware);
+
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
