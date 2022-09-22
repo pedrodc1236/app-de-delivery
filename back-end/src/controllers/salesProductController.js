c0fbaf147f997d575687925c2192e7aa01ce9fbf
evandro663 <evaandro@msn.com>
2022-09-22 17:05:38 -0300

    fix service salesProducts e adicionado mais campos no retorno quando realiza login

--- back-end/src/controllers/salesProductController.js
+++ back-end/src/controllers/salesProductController.js
@@ -1,32 +1,32 @@
-const productService = require('../services/productService');
+const saleProductService = require('../services/salesProductService');
 
 const saleProductController = {
 
   async create(req, res) {
-    const newSaleProduct = await productService.create(req.body);
+    const newSaleProduct = await saleProductService.create(req.body);
     return res.status(201).json(newSaleProduct);
   },
 
   async getAll(req, res) {
-    const salesProducts = await productService.getAll(req.body);
+    const salesProducts = await saleProductService.getAll(req.body);
     return res.status(200).json(salesProducts);
   },
 
   async getById(req, res) {
     const { id } = req.params;
-    const saleProduct = await productService.getById(id);
+    const saleProduct = await saleProductService.getById(id);
     return res.status(200).json(saleProduct);
   },
 
   async update(req, res) {
     const { id } = req.params;
-    const saleProduct = await productService.update(id);
+    const saleProduct = await saleProductService.update(id);
     return res.status(200).json(saleProduct);
   },
 
   async delete(req, res) {
     const { id } = req.param;
-    const saleProduct = await productService.delete(id);
+    const saleProduct = await saleProductService.delete(id);
     return res.status(204).json(saleProduct);
   },
 };
