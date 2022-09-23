8cdc2872c892026db3eec48b36c49d5ea9040446
joaokruschewsky <joaokruschewsky@gmail.com>
2022-09-23 11:28:30 -0300

    localstorage

---
+++ front-end/src/services/localStorage.js
@@ -0,0 +1,38 @@
+const USER = 'user';
+const CART = 'cart';
+// const NAME = 'name';
+const ITEM = 'item';
+
+// add user email
+export const addUser = (name, email, role, token) => {
+  localStorage.setItem(USER, JSON.stringify({ name, email, role, token }));
+};
+
+// get user name localstorage
+export const getUser = () => {
+  const user = JSON.parse(localStorage.getItem(USER)) || { name: '' };
+  return user;
+};
+
+// add fav
+export const addFavorite = (item) => {
+  const items = JSON.parse(localStorage.getItem(CART)) || [];
+  const newItem = [...items, item];
+  localStorage.setItem(CART, JSON.stringify(newItem));
+};
+
+// remove fav
+export const removeFromFavorite = (id) => {
+  const items = JSON.parse(localStorage.getItem(ITEM)) || [];
+  const newItem = items.filter((item) => item.id !== id);
+  localStorage.setItem(ITEM, JSON.stringify(newItem));
+};
+
+// search user item
+export const getItem = () => {
+  const item = JSON.parse(localStorage.getItem(ITEM)) || [];
+  return item;
+};
+
+// limpa o localStorage
+export const cleanLocalStorage = () => localStorage.clear();
